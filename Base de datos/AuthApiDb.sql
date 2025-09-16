963c8c5237d496447281d51dd05a7be56016181c
Rafael Anaya <rafanayomero@gmail.com>
2025-09-16 02:16:34 -0600

    fix: Correcciones necesarias para funcionamiento correcto de base de datos inicial

--- Base de datos/AuthApiDb.sql
+++ Base de datos/AuthApiDb.sql
@@ -1,6 +1,6 @@
-CREATE DATABASE Usuarios 
+CREATE DATABASE AuthApi 
 
-Use Usuarios
+Use AuthApi
 
 
 --Creación de tabla de Usuarios
@@ -20,4 +20,28 @@ CREATE TABLE Usuarios (
     
     CONSTRAINT chk_estado CHECK (Estado IN ('A', 'I'))
 );
+
+
+-- Inserción del primer usuario por defecto
+INSERT INTO Usuarios (
+    Nombres,
+    Apellidos,
+    FechaNacimiento,
+    Direccion,
+    Password,
+    Telefono,
+    Email,
+    FechaCreacion
+)
+VALUES (
+    'Administrador',             -- Nombres
+    'Sistema',                   -- Apellidos
+    '1990-01-01',                -- FechaNacimiento
+    'Dirección del Admin',       -- Direccion
+    '5997441171671814138679518223612922947249451091219013990156237151069920112283178',-- Password (Admin123) 
+    '71727374',                 -- Telefono
+    'admin@authapi.com',         -- Email
+    GETDATE()
+);
+
 
